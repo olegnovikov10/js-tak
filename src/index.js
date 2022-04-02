2f2a1390c608f8d22bbbcc5cbc243833626db2fa
olegnovikov10 <relaxinfh071@gmail.com>
2022-04-03 02:38:08 +0300

    feat: add functional to arhives categories

--- src/index.js
+++ src/index.js
@@ -3,70 +3,97 @@ import '@fortawesome/fontawesome-free/js/fontawesome'
 
 
 window.onload = function () {
-
     let allNotes = []
     let editArr = []
+    let objResult = {}
     let isEdit = false
 
     const title = document.querySelector('.notes-title')
     let select = document.querySelector('.select')
     let options = document.querySelector('.select').options
+    const content = document.querySelector('.content')
+    const categoryContent = document.querySelector('.category__content')
+
+    content.addEventListener('click', (e) => {
+        e.preventDefault();
+        let id = e.target.getAttribute('data-id');
+        if ((e.target.getAttribute('data-type')) === 'edit') {
+            isEdit = true;
+            editNotes(id)
+
+        }
+        if ((e.target.getAttribute('data-type')) === 'delete') {
+            removeNotes(id)
+        }
+        if ((e.target.getAttribute('data-type')) === 'archived') {
+            archivedCategories(id)
+        }
+    })
 
     const editNotes = (id) => {
-         editArr = allNotes.filter((item)=> item.id == id)
+        editArr = allNotes.filter((item) => item.id == id)
         title.value = editArr[0].title;
         select.value = editArr[0].categoryText
     }
-
+    const archivedCategories = (id) => {
+        allNotes = allNotes.map(item => {
+            if (item.id == id) {
+                item.arhived = !item.arhived
+                return item
+            }
+            return item
+        })
+        render()
+        renderCategories()
+    }
     const removeNotes = (id) => {
         const res = allNotes.filter((item) => {
             return item.id != id
         })
         allNotes = [...res]
         render()
+        renderCategories()
     }
-
-
-
-    const addNotes = (title = 'exmaple text', category = "text" , code, id=null) => {
-
+    const addNotes = (title = 'exmaple text', category = "text", code, id = null) => {
         if (title.length < 2) {
             return false
         }
-
-        if(!isEdit){
+        if (!isEdit) {
             const date = new Date().toLocaleString('en', {month: 'short', year: 'numeric', day: 'numeric'})
             allNotes.push({
                 id: Date.now(),
                 title: title,
                 category: category,
-                categoryText : code,
-                date: date
+                categoryText: code,
+                date: date,
+                arhived: false
             })
         }
 
-        if(isEdit){
+        if (isEdit) {
             const date = new Date().toLocaleString('en', {month: 'short', year: 'numeric', day: 'numeric'})
-            allNotes = allNotes.map((item)=>{
-                if(item.id == editArr[0].id){
+            allNotes = allNotes.map((item) => {
+                if (item.id == editArr[0].id) {
                     const obj = {
                         ...editArr[0],
                         title: title,
                         category: category,
                         date: date,
-                        categoryText : code,
+                        categoryText: code,
                     }
                     return obj
-                }else {
+                } else {
                     return item
                 }
+
+
             })
 
             render()
+
             isEdit = false
             editArr = null
         }
-
 
 
     }
@@ -77,34 +104,19 @@ window.onload = function () {
 
         if (title.value.length < 2) {
             alert('More text please')
+            return false
         }
-            addNotes(title.value, options[select.selectedIndex].text , select.value)
+        addNotes(title.value, options[select.selectedIndex].text, select.value)
 
         title.value = ''
         select.value = 'task'
         render()
+        renderCategories()
 
     }
-    const content = document.querySelector('.content')
-    content.addEventListener('click', (e) => {
-        e.preventDefault();
-        e.stopPropagation()
-        let id = e.target.getAttribute('data-id');
-        if( (e.target.getAttribute('data-type')) === 'edit'){
-            isEdit = true;
-            editNotes(id)
 
-        }
-        if( (e.target.getAttribute('data-type')) === 'delete'){
-            removeNotes(id)
-
-        }
-
-
-    })
 
     const render = () => {
-        console.log(allNotes)
         content.innerHTML = ''
         if (allNotes.length > 0) {
             allNotes.forEach((item) => {
@@ -117,7 +129,7 @@ window.onload = function () {
                 <div>
                     <button data-type="delete" data-id=${item.id}>delete</button>
                     <button data-type="edit" data-id=${item.id}>Edit</button>
-                    
+                    <button data-type="archived" data-id=${item.id}>Arhived</button>
                  </div>
                 
                </div>`)
@@ -125,6 +137,61 @@ window.onload = function () {
 
         }
         return false
+    }
+
+    const countCategories = () => {
+        objResult = {
+            Task: {
+                activeCount: 0,
+                archiveCount: 0
+            },
+            'Random Thought': {
+                activeCount: 0,
+                archiveCount: 0
+            },
+            'Idea': {
+                activeCount: 0,
+                archiveCount: 0
+            },
+            'Quote': {
+                activeCount: 0,
+                archiveCount: 0
+            }
+        }
+
+        if (allNotes.length > 0) {
+            Object.keys(objResult).forEach((categories) => {
+                allNotes.forEach((item) => {
+                    if (categories == item.category) {
+                        if (item.arhived) {
+                            objResult[categories].archiveCount += 1
+                        } else {
+                            objResult[categories].activeCount += 1
+                        }
+                    }
+                })
+
+            })
+        }
+    }
+    const renderCategories = () => {
+        categoryContent.innerHTML = ''
+        countCategories();
+
+        for (let key in objResult) {
+            if (objResult.hasOwnProperty(key)) {
+
+                categoryContent.insertAdjacentHTML(
+                'beforeend',
+                    `<div class="category__items">
+                        <div class="category__item">${key}</div>
+                        <div class="category__item">${objResult[key].activeCount}</div>
+                        <div class="category__item">${objResult[key].archiveCount}</div>
+                      </div>`)
+
+            }
+        }
+
     }
 }
 
