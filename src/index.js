a373e35854e56a8dd3eefab29f453a42d04cc161
olegnovikov10 <relaxinfh071@gmail.com>
2022-04-03 22:26:09 +0300

    feat : add desing

--- src/index.js
+++ src/index.js
@@ -1,19 +1,46 @@
-import './style.css'
-import '@fortawesome/fontawesome-free/js/fontawesome'
-
+import './styles.scss'
+import 'material-icons/iconfont/material-icons.css';
 
 window.onload = function () {
     let allNotes = []
     let editArr = []
     let objResult = {}
     let isEdit = false
+    const regDate = '^([0-2][0-9]||3[0-1])/(0[0-9]||1[0-1])/([0-9][0-9])?[0-9][0-9]$'
 
+    const nameNotes = document.querySelector('.notes-name')
     const title = document.querySelector('.notes-title')
     let select = document.querySelector('.select')
     let options = document.querySelector('.select').options
     const content = document.querySelector('.content')
-    const categoryContent = document.querySelector('.category__content')
+    const categoryArhive = document.querySelector('.category-arhive')
+    const isOpenFrom = document.querySelector('#isOpenFrom')
+    const closeForm = document.querySelector('#closeForm')
+    const myForm = document.querySelector('.form-wrapper')
+    const categoryContent = document.querySelector('#category__content-count')
+    const notesArchive = document.querySelector('#notes-archive')
 
+    // закрываем форму
+    closeForm.addEventListener('click', (e) => {
+        e.preventDefault()
+        myForm.classList.add('hidden')
+
+    })
+    // показываем форму
+    isOpenFrom.addEventListener('click', (e) => {
+            e.preventDefault()
+            myForm.classList.remove('hidden')
+        }
+    )
+
+    // удаление из архива
+    categoryArhive.addEventListener('click', (e)=>{
+        let id = e.target.getAttribute('data-id');
+        if ((e.target.getAttribute('data-type')) === 'unarchive') {
+            unarchive(id)
+        }
+    })
+    // добавление событие кликов (  редактировать , удалить , архивировать )
     content.addEventListener('click', (e) => {
         e.preventDefault();
         let id = e.target.getAttribute('data-id');
@@ -28,24 +55,45 @@ window.onload = function () {
         if ((e.target.getAttribute('data-type')) === 'archived') {
             archivedCategories(id)
         }
-    })
 
+    })
+    //заполняем форму содержимом для редактировании
     const editNotes = (id) => {
+
         editArr = allNotes.filter((item) => item.id == id)
         title.value = editArr[0].title;
+        nameNotes.value = editArr[0].nameNotes;
         select.value = editArr[0].categoryText
+        myForm.classList.remove('hidden')
     }
-    const archivedCategories = (id) => {
+
+    //удаляем из архива
+    const unarchive = (id) => {
         allNotes = allNotes.map(item => {
             if (item.id == id) {
-                item.arhived = !item.arhived
+                item.archive = !item.archive
                 return item
             }
             return item
         })
         render()
         renderCategories()
+        renderArchives()
     }
+    // Добавляем в архив запись
+    const archivedCategories = (id) => {
+        allNotes = allNotes.map(item => {
+            if (item.id == id) {
+                item.archive = !item.archive
+                return item
+            }
+            return item
+        })
+        render()
+        renderCategories()
+        renderArchives()
+    }
+    // функция удаления записи
     const removeNotes = (id) => {
         const res = allNotes.filter((item) => {
             return item.id != id
@@ -54,32 +102,49 @@ window.onload = function () {
         render()
         renderCategories()
     }
-    const addNotes = (title = 'exmaple text', category = "text", code, id = null) => {
+    //доавление или редактивоние записи
+    const addNotes = (nameNotes = '', title = 'exmaple text', category = "text", code, id = null) => {
         if (title.length < 2) {
             return false
         }
+        let datesArr = []
+        let reg = /[\r\n]+/g;
+
+        let titleSplit = title.replace(reg," ").split(' ')
+        titleSplit.forEach((item) => {
+            if (item.match(regDate)) {
+                datesArr.push(item)
+            }
+        })
+
         if (!isEdit) {
+
             const date = new Date().toLocaleString('en', {month: 'short', year: 'numeric', day: 'numeric'})
             allNotes.push({
                 id: Date.now(),
+                nameNotes: nameNotes,
                 title: title,
                 category: category,
                 categoryText: code,
-                date: date,
-                arhived: false
+                created: date,
+                archive: false,
+                dates: datesArr.join(' ')
             })
         }
 
         if (isEdit) {
             const date = new Date().toLocaleString('en', {month: 'short', year: 'numeric', day: 'numeric'})
             allNotes = allNotes.map((item) => {
+
                 if (item.id == editArr[0].id) {
                     const obj = {
                         ...editArr[0],
+                        nameNotes: nameNotes,
                         title: title,
                         category: category,
-                        date: date,
+                        created: date,
                         categoryText: code,
+                        dates: datesArr.join(' ')
                     }
                     return obj
                 } else {
@@ -97,39 +162,44 @@ window.onload = function () {
 
 
     }
-
+    // собтыие на клик формы.
     const btn = document.querySelector('.sendForm')
     btn.onclick = function (e) {
         e.preventDefault()
-
         if (title.value.length < 2) {
             alert('More text please')
             return false
         }
-        addNotes(title.value, options[select.selectedIndex].text, select.value)
+        addNotes(nameNotes.value, title.value, options[select.selectedIndex].text, select.value)
 
+        nameNotes.value = ''
         title.value = ''
         select.value = 'task'
+        myForm.classList.add('hidden')
         render()
         renderCategories()
 
     }
 
-
+    // рендер записей
     const render = () => {
         content.innerHTML = ''
         if (allNotes.length > 0) {
-            allNotes.forEach((item) => {
+            allNotes.filter(it => it.archive == false).forEach((item) => {
                 content.insertAdjacentHTML('beforeend',
                     `<div class="content__item">
-                <div class="content__name"> ${item.title} </div>
+                <div class="content__name"> ${item.nameNotes} </div>
+                <div class="content__name"> ${item.created} </div>
                 <div class="content__name"> ${item.category} </div>
-                <div class="content__name"> ${item.date} </div>
+                <div class="content__name"> ${item.title} </div>
+              
+               
+                <div class="content__name"> ${item.dates.length > 0 ? item.dates : ""} </div>
                 
-                <div>
-                    <button data-type="delete" data-id=${item.id}>delete</button>
-                    <button data-type="edit" data-id=${item.id}>Edit</button>
-                    <button data-type="archived" data-id=${item.id}>Arhived</button>
+                <div class="content__name">
+                    <span class="material-icons links" data-type="edit" data-id=${item.id}>edit</span>
+                    <span class="material-icons links" data-type="archived" data-id=${item.id}>archive</span>
+                    <span class="material-icons links" data-type="delete" data-id=${item.id}>delete</span>
                  </div>
                 
                </div>`)
@@ -138,7 +208,7 @@ window.onload = function () {
         }
         return false
     }
-
+    // считаем записи активных и архивных
     const countCategories = () => {
         objResult = {
             Task: {
@@ -163,7 +233,7 @@ window.onload = function () {
             Object.keys(objResult).forEach((categories) => {
                 allNotes.forEach((item) => {
                     if (categories == item.category) {
-                        if (item.arhived) {
+                        if (item.archive) {
                             objResult[categories].archiveCount += 1
                         } else {
                             objResult[categories].activeCount += 1
@@ -174,6 +244,7 @@ window.onload = function () {
             })
         }
     }
+    // рендер количество активных и архивных категорий
     const renderCategories = () => {
         categoryContent.innerHTML = ''
         countCategories();
@@ -182,7 +253,7 @@ window.onload = function () {
             if (objResult.hasOwnProperty(key)) {
 
                 categoryContent.insertAdjacentHTML(
-                'beforeend',
+                    'beforeend',
                     `<div class="category__items">
                         <div class="category__item">${key}</div>
                         <div class="category__item">${objResult[key].activeCount}</div>
@@ -193,6 +264,26 @@ window.onload = function () {
         }
 
     }
+    renderCategories()
+    const renderArchives = () => {
+        notesArchive.innerHTML = ''
+        if (allNotes.length > 0) {
+            allNotes.filter(it => it.archive == true).forEach((item) => {
+                notesArchive.insertAdjacentHTML('beforeend',
+                    `<div class="content__item">
+                <div class="content__name"> ${item.nameNotes} </div>
+                <div class="content__name"> ${item.category} </div>
+                <div class="content__name"> ${item.title} </div>
+                <div class="content__name">
+                    <span class="material-icons links"  data-type="unarchive" data-id=${item.id}>unarchive</span>
+                 </div>
+               </div>`)
+            })
+
+        }
+        return false
+    }
+
 }
 
 
